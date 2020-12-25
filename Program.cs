40b3d1193c4c15abbf8b632823188a9251bfdfa4
kumarmo2 <mohitkumar1015795@gmail.com>
2020-12-25 18:06:19 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace DynamicDispatchCostDemo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      var list = new List<string>();
+      var set = new HashSet<string>();
+
+      for (var i = 0; i < 10; i++)
+      {
+        set.Add(i.ToString());
+      }
+      DynamicDispatchExample(set);
+
+      for (var i = 0; i < 10000000; i++)
+      {
+        list.Add(i.ToString());
+      }
+      DynamicDispatchExample(list);
+      //   StaticDispatchExample(list);
+    }
+
+    private static int DynamicDispatchExample(IEnumerable<string> list)
+    {
+      var stopWatch = Stopwatch.StartNew();
+      var sum = 0;
+
+      foreach (var ele in list)
+      {
+        sum += 1;
+      }
+      Console.WriteLine($"{stopWatch.ElapsedMilliseconds} for dynamic dispatch");
+      return sum;
+
+    }
+    private static int StaticDispatchExample(List<string> list)
+    {
+      var stopWatch = Stopwatch.StartNew();
+      var sum = 0;
+      for (var i = 0; i < list.Count; i++)
+      {
+        sum += 1;
+      }
+      Console.WriteLine($"{stopWatch.ElapsedMilliseconds} for static dispatch");
+      return sum;
+    }
+
+
+  }
+}
